fc428554b526df52ed4dd3ed1b360c21dfa29ff9
Hkumasu <116594262+Hkumasu@users.noreply.github.com>
2023-04-06 23:38:43 -0700

    Update ResumeCommand.java

--- src/simstation/ResumeCommand.java
+++ src/simstation/ResumeCommand.java
@@ -1,5 +1,6 @@
 /*
 4/4/23 - Hazuki Sugahara: Created the file
+4/6/23 - Hazuki Sugahara: Modified the file
 */
 
 package simstation;
@@ -7,5 +8,12 @@ package simstation;
 import mvc.*;
 
 public class ResumeCommand extends Command {
-  
+  public ResumeCommand(Model model) {
+        super(model);
+    }
+
+    public void execute() {
+        Simulation m = (Simulation)model;
+        m.resume();
+    }
 }
