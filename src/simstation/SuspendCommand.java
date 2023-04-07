5e0ecd8d92c29f5353e4fedf751e1e7005eecfe5
Hkumasu <116594262+Hkumasu@users.noreply.github.com>
2023-04-06 23:45:19 -0700

    Update SuspendCommand.java

--- src/simstation/SuspendCommand.java
+++ src/simstation/SuspendCommand.java
@@ -1,5 +1,6 @@
 /*
 4/4/23 - Hazuki Sugahara: Created the file
+4/4/23 - Hazuki Sugahara: Modified the file
 */
 
 package simstation;
@@ -7,5 +8,12 @@ package simstation;
 import mvc.*;
 
 public class SuspendCommand extends Command {
-  
+  public SuspendCommand(Model model) {
+        super(model);
+    }
+
+    public void execute() {
+        Simulation m = (Simulation)model;
+        m.suspend();
+    }
 }
