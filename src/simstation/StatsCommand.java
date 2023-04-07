13aa1562cbbdd448629ea56cf46d73b837f278e6
Hkumasu <116594262+Hkumasu@users.noreply.github.com>
2023-04-06 23:39:45 -0700

    Update StatsCommand.java

--- src/simstation/StatsCommand.java
+++ src/simstation/StatsCommand.java
@@ -1,5 +1,6 @@
 /*
 4/4/23 - Hazuki Sugahara: Created the file
+4/6/23 - Hazuki Sugahara: Modified the file
 */
 
 package simstation;
@@ -7,5 +8,12 @@ package simstation;
 import mvc.*;
 
 public class StatsCommand extends Command {
-  
+  public StatsCommand(Model model) {
+        super(model);
+    }
+
+    public void execute() {
+        Simulation m = (Simulation)model;
+        m.populate();
+    }
 }
