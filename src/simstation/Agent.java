e4402509853603456481d7b00110b083c84d1349
Hkumasu <116594262+Hkumasu@users.noreply.github.com>
2023-04-06 23:43:10 -0700

    Update Agent.java

--- src/simstation/Agent.java
+++ src/simstation/Agent.java
@@ -1,5 +1,6 @@
 /*
 4/4/23 - Hazuki Sugahara: Created the file
+4/6/23 - Hazuki Sugahara: copied the agentlab and modifiying for simstation
 */
 
 package simstation;
@@ -9,5 +10,65 @@ import java.io.Serializable;
 
 
 abstract class Agent implements Serializable, Runnable {
-  
+  protected String name;
+    protected Thread myThread;
+    private boolean suspended, stopped;
+    protected Simulation Simulation;
+
+    public Agent(String name) {
+        this.name = name;
+        suspended = false;
+        stopped = false;
+        myThread = null;
+    }
+
+    public void setSimulation(Simulation s) { Simulation = s; }
+    public String getName() { return name; }
+    public synchronized String toString() {
+        String result = name;
+        if (stopped) result += " (stopped)";
+        else if (suspended) result += " (suspended)";
+        else result += " (running)";
+        return result;
+    }
+    // thread stuff:
+    public synchronized void stop() { stopped = true; }
+    public synchronized boolean isStopped() { return stopped; }
+    public synchronized void suspend() { suspended = true; }
+    public synchronized boolean isSuspended() { return suspended;  }
+    public synchronized void resume() { notify(); }
+    public synchronized void join() {
+        try {
+            if (myThread != null) myThread.join();
+        } catch(InterruptedException e) {
+            manager.println(e.getMessage());
+        }
+    }
+    private synchronized void checkSuspended() {
+        try {
+            while(!stopped && suspended) {
+                wait();
+                suspended = false;
+            }
+        } catch (InterruptedException e) {
+            manager.println(e.getMessage());
+        }
+    }
+
+
+    public void run() {
+        myThread = Thread.currentThread();
+        while (!isStopped()) {
+            try {
+                update();
+                Thread.sleep(1000);
+                checkSuspended();
+            } catch(InterruptedException e) {
+                manager.println(e.getMessage());
+            }
+        }
+        manager.stdout.println(name + " stopped");
+    }
+
+    public abstract void update();
 }
