75bdb048cfa43deb45e6dd80b8617857ce8bab63
Hkumasu <116594262+Hkumasu@users.noreply.github.com>
2023-04-06 23:40:27 -0700

    Update StopCommand.java

--- src/simstation/StopCommand.java
+++ src/simstation/StopCommand.java
@@ -1,5 +1,6 @@
 /*
 4/4/23 - Hazuki Sugahara: Created the file
+4/6/23 - Hazuki Sugahara: Modified the file
 */
 
 package simstation;
@@ -7,5 +8,12 @@ package simstation;
 import mvc.*;
 
 public class StopCommand extends Command {
-  
+  public StopCommand(Model model) {
+        super(model);
+    }
+
+    public void execute() {
+        Simulation m = (Simulation)model;
+        m.stop();
+    }
 }
