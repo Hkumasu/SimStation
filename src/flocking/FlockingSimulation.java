a8ed3489f0f80a6c0364e87a3087a570e743429c
Hkumasu <116594262+Hkumasu@users.noreply.github.com>
2023-04-13 10:39:23 -0700

    Create FlockingSimulation.java

---
+++ src/flocking/FlockingSimulation.java
@@ -0,0 +1,25 @@
+/*
+4/13/2023 - Hazuki Sugahara: Created the file 
+*/
+
+package flocking;
+
+import mvc.*;
+import simstation.*;
+import java.awt.*;
+
+Class FlockingSimulation extends Simulation {
+  
+  public void populate() {
+   //add statement 
+  }
+  public static void main(String[] args) {
+    AppPanel panel = new SimulationPanel(new Flockingfactory());
+    panel.display();
+  }
+}
+
+class FlockingFactory extends SimulationFactory {
+  public Model makeModel() { return new FlockingSimulation(); }
+  public String getTitle() { return "Flocking"; }
+}
