edb8262bc8488cc84c296d7d8b351143a394df74
Hkumasu <116594262+Hkumasu@users.noreply.github.com>
2023-04-13 20:04:32 -0700

    Update FlockingSimulation.java

--- src/flocking/FlockingSimulation.java
+++ src/flocking/FlockingSimulation.java
@@ -6,25 +6,45 @@ package flocking;
 
 import mvc.*;
 import simstation.*;
-import java.awt.*;
+import java.util.List;
 
 public class FlockingSimulation extends Simulation {
-  
-  public void populate() {
-   //add statement 
-  }
-  
-  public String getStatus() {
-  
-  }
-  
-  public static void main(String[] args) {
-    AppPanel panel = new SimulationPanel(new Flockingfactory());
-    panel.display();
-  }
+
+    public void populate() {
+        for(int i = 0; i < 15; i++)
+            add(new Bird());
+    }
+
+    public void stats() {
+        int speed1 = 0, speed2 = 0, speed3 = 0, speed4 = 0, speed5 = 0, speed = 0;
+        List<Agent> agents = this.getAgents();
+        for(Agent a: agents) {
+            speed = a.getSpeed();
+            switch (speed) {
+                case 1: speed1++;
+                break;
+                case 2: speed2++;
+                break;
+                case 3: speed3++;
+                break;
+                case 4: speed4++;
+                break;
+                case 5: speed5++;
+                break;
+            }
+        }
+
+        Utilities.inform("#birds @ speed 1 = " + speed1 + "\n" + "#birds @ speed 2 = " + speed2 + "\n" +
+                "#birds @ speed 3 = " + speed3 + "\n" + "#birds @ speed 4 = " + speed4 + "\n" + "#birds @ speed 5 = " + speed5);
+    }
+
+    public static void main(String[] args) {
+        AppPanel panel = new SimulationPanel(new FlockingFactory());
+        panel.display();
+    }
 }
 
 class FlockingFactory extends SimStationFactory {
-  public Model makeModel() { return new FlockingSimulation(); }
-  public String getTitle() { return "Flocking"; }
+    public Model makeModel() { return new FlockingSimulation(); }
+    public String getTitle() { return "Flocking"; }
 }
