9b0bc98b87156e1b844687df8ab7bddf6e9c937a
Hkumasu <116594262+Hkumasu@users.noreply.github.com>
2023-04-13 20:03:25 -0700

    Update Bird.java

--- src/flocking/Bird.java
+++ src/flocking/Bird.java
@@ -8,16 +8,21 @@ import mvc.*;
 import simstation;
 
 class Bird extends Agent {
-  
-  int speed;
-  
-  public Bird() {
-    super();
-    heading = Heading.random();
-    speed = utilities.rng.nextInt(5) + 1;
-  }
-  
-  public void update() {
-    //add statement
-  }
-    
+
+//    int speed;
+
+    public Bird() {
+        super();
+        heading = Heading.random();
+        speed = Utilities.rng.nextInt(5) + 1;
+    }
+
+    public void update() {
+        Agent neighbor = simulation.getneightbor(this, 10);
+        if (neighbor != null) {
+            heading = neighbor.getHeading();
+            speed = neighbor.getSpeed();
+        }
+        move(speed);
+    }
+}
