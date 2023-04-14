e047c2336311303c48aaefd4c918d73b9f6d4f4f
Hkumasu <116594262+Hkumasu@users.noreply.github.com>
2023-04-13 20:52:20 -0700

    Create PlagueSimulation.java

---
+++ src/plague/PlagueSimulation.java
@@ -0,0 +1,9 @@
+package plague;
+
+import simstation.*;
+
+public class PlagueSimulation extends Simulation {
+    public static int VIRULENCE = 50; //% chance of infection
+    public static int RESISTANCE = 2; //% chance of resisting infection
+  
+}
