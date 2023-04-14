0c4276a3f89786238ef806b36c15ce10ed7a35cb
Hkumasu <116594262+Hkumasu@users.noreply.github.com>
2023-04-13 20:04:59 -0700

    Update Bird.java

--- src/flocking/Bird.java
+++ src/flocking/Bird.java
@@ -6,45 +6,22 @@ package flocking;
 
 import mvc.*;
 import simstation;
-import java.util.List;
+class Bird extends Agent {
 
-public class FlockingSimulation extends Simulation {
+//    int speed;
 
-    public void populate() {
-        for(int i = 0; i < 15; i++)
-            add(new Bird());
+    public Bird() {
+        super();
+        heading = Heading.random();
+        speed = Utilities.rng.nextInt(5) + 1;
     }
 
-    public void stats() {
-        int speed1 = 0, speed2 = 0, speed3 = 0, speed4 = 0, speed5 = 0, speed = 0;
-        List<Agent> agents = this.getAgents();
-        for(Agent a: agents) {
-            speed = a.getSpeed();
-            switch (speed) {
-                case 1: speed1++;
-                break;
-                case 2: speed2++;
-                break;
-                case 3: speed3++;
-                break;
-                case 4: speed4++;
-                break;
-                case 5: speed5++;
-                break;
-            }
+    public void update() {
+        Agent neighbor = simulation.getneightbor(this, 10);
+        if (neighbor != null) {
+            heading = neighbor.getHeading();
+            speed = neighbor.getSpeed();
         }
-
-        Utilities.inform("#birds @ speed 1 = " + speed1 + "\n" + "#birds @ speed 2 = " + speed2 + "\n" +
-                "#birds @ speed 3 = " + speed3 + "\n" + "#birds @ speed 4 = " + speed4 + "\n" + "#birds @ speed 5 = " + speed5);
+        move(speed);
     }
-
-    public static void main(String[] args) {
-        AppPanel panel = new SimulationPanel(new FlockingFactory());
-        panel.display();
-    }
-}
-
-class FlockingFactory extends SimStationFactory {
-    public Model makeModel() { return new FlockingSimulation(); }
-    public String getTitle() { return "Flocking"; }
 }
